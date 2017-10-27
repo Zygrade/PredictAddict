8afa26522d38183068ad8ba22d5a7b947b2a3c0a
Yubhrraj Pathak <yubhrraj.p@gmail.com>
2017-10-28 05:17:40 +0530

    Android 2nd commit

--- AndroidApp/PredictAddict/app/src/main/java/com/hashhacks/predictaddict/MainActivity.java
+++ AndroidApp/PredictAddict/app/src/main/java/com/hashhacks/predictaddict/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hashhacks.predictaddict;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void pressOrder(View view){
-
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, StartTest.class);
+        startActivity(intent);
     }
 }
